967f900473f891bef642a79ee18196651b88cf21
michal <karalusmichal@gmail.com>
2020-10-08 19:52:01 +0200

    Update task endpoint

--- to_do/serializers.py
+++ to_do/serializers.py
@@ -6,4 +6,4 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Task
-        fields = ['task_id', 'title', 'description', 'priority']
+        fields = ['task_id', 'title', 'description', 'priority', 'inserted_at']
