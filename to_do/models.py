811b0f3c140f67bb045eaf46814a125f68bfffda
michal <karalusmichal@gmail.com>
2020-10-08 18:46:06 +0200

    Add task model

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
+from django.utils import timezone
+from colorfield.fields import ColorField
 
-# Create your models here.
+
+class Task(models.Model):
+
+    task_id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    priority = ColorField(default='#FF0000')
+    inserted_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "tasks"
