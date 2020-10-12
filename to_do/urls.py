0aa6697543ba7cb3a87f51b2c2079ea908d85cef
michal <karalusmichal@gmail.com>
2020-10-12 18:56:34 +0200

    Add task get, patch and delete methods

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from to_do import views
 
 urlpatterns = [
     path('tasks/', views.TasksViewSet.as_view(), name='tasks'),
+    path('tasks/<uuid:pk>', views.TaskViewSet.as_view(), name='task')
 ]
--- to_do/views.py
+++ to_do/views.py
@@ -1,5 +1,5 @@
 from .models import Task
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from .serializers import TaskSerializer
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,10 +12,47 @@ class TasksViewSet(ListCreateAPIView):
     def get_queryset(self):
         tasks = Task.objects.all()
         return tasks
-    
+
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskViewSet(RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            content = {
+                'status': 'Not Found'
+            }
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        return task
+
+    def get(self, request, pk):
+
+        task = self.get_queryset(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        
+        task = self.get_queryset(pk)
+        serializer = TaskSerializer(task, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, pk):
+
+        task = self.get_queryset(pk)
+        if request:
+            task.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
